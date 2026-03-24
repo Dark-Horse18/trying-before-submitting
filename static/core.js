3ec92b29e1d7d097ad4d8624ab4f1ca3a35e75d9
kanixx294 <kanishksingh5102007@gmail.com>
2026-03-24 22:43:31 +0530

    Snake simulated, added arrow key movement, added gameover logic

--- static/core.js
+++ static/core.js
@@ -2,11 +2,19 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
 // 1. INITIALIZE STATE
-let player = { x: 250, y: 250, width: 25, height: 25, speed: 25, dir_x: 0, dir_y: 0 };
+let player = { x: 250, y: 250, width: 25, height: 25, speed: 25, dir_x: 0, dir_y: 0 }; // snake head(basically snake[0])
 let keys = {};
+msg = "";
+snake = [[250, 250]];
+state = "START";
+// fruit_carrot = [Math.floor(Math.random() * 20) * 25, Math.floor(Math.random() * 20) * 25]
+// fruit_pumkin = [Math.floor(Math.random() * 20) * 25, Math.floor(Math.random() * 20) * 25]
+// fruit_apple = [Math.floor(Math.random() * 20) * 25, Math.floor(Math.random() * 20) * 25]
 
 const headImg = new Image();
 headImg.src = "hq720.jpg"
+const bodyImg = new Image();
+bodyImg.src = "goku.jpg.jpeg"
 
 
 // 2. INPUT HANDLING
@@ -19,27 +27,44 @@ const FPS = 10; // Set your desired Frames Per Second
 const frameDelay = 1000 / FPS; // 1000ms divided by 10 FPS = 100ms delay
 
 function gameLoop() {
-    update();
-    draw();
+
+	if (state == "START") {
+		alert("Press Ok to start")
+		state = "PLAYING"
+	}
+	else if (state == "PLAYING") {
+		update();
+		draw();
+		gameOver();
+	}
+	else if (state == "OVER") {
+		alert(msg);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		msg = "";
+		state = "START";
+		player = { x: 250, y: 250, width: 25, height: 25, speed: 25, dir_x: 0, dir_y: 0 };
+		keys = {};
+		snake = [[250, 250]];
+	}
 }
 
 // This forces the loop to run exactly every 'frameDelay' milliseconds
 setInterval(gameLoop, frameDelay);
 
 function update() {
-	if (keys['KeyW'] && player.dir_y == 0) {
+	if ((keys['KeyW'] || keys['ArrowUp']) && player.dir_y == 0) {
 		player.dir_x = 0;
 		player.dir_y = -1;
 	}
-	if (keys['KeyS'] && player.dir_y == 0) {
+	if ((keys['KeyS'] || keys['ArrowDown']) && player.dir_y == 0) {
 		player.dir_x = 0;
 		player.dir_y = 1;
 	}
-	if (keys['KeyA'] && player.dir_x == 0) {
+	if ((keys['KeyA'] || keys['ArrowLeft']) && player.dir_x == 0) {
 		player.dir_x = -1;
 		player.dir_y = 0;
 	}
-	if (keys['KeyD'] && player.dir_x == 0) {
+	if ((keys['KeyD'] || keys['ArrowRight']) && player.dir_x == 0) {
 		player.dir_x = 1;
 		player.dir_y = 0;
 	}
@@ -47,10 +72,9 @@ function update() {
 	player.x += player.dir_x * player.speed
 	player.y += player.dir_y * player.speed
 
-
-	// // Basic Screen Boundary Check
-	// if (player.x < 0) player.x = 0;
-	// if (player.x + player.width > canvas.width) player.x = canvas.width - player.width;
+	snake.unshift([player.x, player.y]);
+	snake.pop();
+	
 }
 
 function draw() {
@@ -61,7 +85,37 @@ function draw() {
 	drawGrid();
 
 	// Draw Player
-	ctx.drawImage(headImg, player.x, player.y, player.width, player.height);
+	for(let i = 1; i < snake.length; i++) {
+		ctx.drawImage(bodyImg, snake[i][0], snake[i][1], player.width, player.height);
+	}
+	ctx.drawImage(headImg, snake[0][0], snake[0][1], player.width, player.height);
+}
+
+function gameOver() {
+	if (player.x < 0) {
+		msg = "Wall Collision";
+		state = "OVER";
+	}
+	if (player.x + player.width > canvas.width) {
+		msg = "Wall Collision";
+		state = "OVER";
+	}
+	if (player.y < 0) {
+		msg = "Wall Collision";
+		state = "OVER";
+	}
+	if (player.y + player.width > canvas.height) {
+		msg = "Wall Collision";
+		state = "OVER";
+	}
+
+	for (let i = 1; i < snake.length; i++) {
+		if (snake[0][0] == snake[i][0] && snake[0][1] == snake[i][1]) {
+			msg = "Self Collision";
+			state = "OVER";
+			break;
+		}
+	}
 }
 
 function drawGrid() {
